eb3e8d33a9a541fb735a8b49cc19f5437959dcf5
ahmedayman6038 <ahmedayman6038@gmail.com>
2020-12-28 21:49:57 +0200

    12282020

---
+++ WebSPA/Models/JobModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebSPA.Models
+{
+    public class JobModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Requirements { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+    }
+}
